e7f27bd889826576e3669175ec87dced61eb6868
eskay16 <odefemiayomide@gmail.com>
2025-10-27 17:41:28 +0200

    implement simple server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
- 
+ import express from "express";
+
+ const app = express();
+
+
+app.use(express.json());
+
+app.use('/home', (req, res) =>{
+    res.json({message: "Welcome"});
+});
+
+app.listen(4000, ()=>{
+    console.log("http://localhost:4000");
+});
