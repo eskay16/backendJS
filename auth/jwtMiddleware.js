3fa06f24702b9e464ef21356d673e372b379e3a4
eskay16 <odefemiayomide@gmail.com>
2025-11-11 15:31:35 +0200

    jwtMiddleware

--- auth/jwtMiddleware.js
+++ auth/jwtMiddleware.js
@@ -1,27 +1,23 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET
 
-export const jwtMiddleware = (req, res, next)=>{
+export const jwtMiddleware = (req, res, next) => {
+  try {
+    const token = req.cookies['bj']
+    if (!token) return res.status(400).json({ message: "auth token missing." });
 
-    try {
-        
-        const token = req.cookies['customer'];
-        if(!token) {
-            return res.status(400).json({Message: "authentication missing"})};
+    const decoded = jwt.verify(token, JWT_SECRET)
+    console.log(decoded)
 
-            const decoded = jwt.verify(token, JWT_SECRET);
-            console.log(decoded);
-
-            if('userName' in decoded){
-                console.log(decoded.userName);
-                req.user = decoded;
-                return next();
-            }
-
-            return res.status(400).json({Message: "invalid token"});
-
-    } catch (err) {
-        return res.status(400).json({Message: "something went wrong "+ err.message});
+    if ('userName' in decoded) {
+      console.log(decoded.userName);
+      return next()
     }
+
+    return res.status(400).json({ message: "invalid token." })
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({ message: "Something went wrong" })
+  }
 }
